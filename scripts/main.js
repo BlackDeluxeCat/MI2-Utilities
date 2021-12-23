a505f28783d96d0ea402432de68ed3a77c70aaaa
BlackDeluxeCat <65377021+BlackDeluxeCat@users.noreply.github.com>
2021-12-23 14:43:12 +0800

    dragable ui and other changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,41 @@
 require("MI2Settings")
+const version = "0.1.0";
 
 
-Time.run(1,run(() => {
+Events.on(EventType.ClientLoadEvent, e => {
+    const UIHudGroup = Vars.ui.hudGroup;
     const dragTable = extend(Table, {
-        curx : 50, cury : 10, flip : true
+        curx : 100, cury : 200, flip : true, fromx : 0, fromy : 0
     });
-    dragTable.left().bottom().marginLeft(dragTable.curx).marginRight(dragTable.cury);
-    dragTable.label(() => "MI2U");
+    dragTable.name("MI2U_Main")
+    UIHudGroup.addChild(dragTable);
+
+    dragTable.left().bottom().setFillParent(true);
+    dragTable.update(() => {
+        dragTable.setPosition(dragTable.curx, dragTable.cury);
+    });
+    
+
+    var titleLabel = new Label("MI2U");
+    titleLabel.addListener(extend(InputListener, {
+        touchDown(event, x, y, pointer, button){
+            dragTable.fromx = x;
+            dragTable.fromy = y;
+            return true;
+        },
+        touchDragged(event, x, y, pointer){
+            let v = dragTable.localToStageCoordinates(Tmp.v1.set(x, y));
+            dragTable.curx = v.x - dragTable.fromx;
+            dragTable.cury = v.y - dragTable.fromy;
+        }
+    }));
+    
+    dragTable.add(titleLabel);
     dragTable.row();
-    dragTable.label(() => "0.1.0");
+    dragTable.label(() => "" + version);
     dragTable.row();
     dragTable.button(Icon.refreshSmall, () => {
         Call.sendChatMessage("/sync");
-    }).maxSize(36, 36);
-    Core.scene.add(dragTable);
-}));
+    }).size(36, 36);
+    
+});
