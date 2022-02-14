811c164685fd7351c29431cae4fc10a422acb284
BlackDeluxeCat <65377021+BlackDeluxeCat@users.noreply.github.com>
2022-02-14 13:00:00 +0800

    new hp bar

--- scripts/healthBar.js
+++ scripts/healthBar.js
@@ -34,24 +34,25 @@ function draw(unit){
         unit.x + unit.hitSize * ((unit.health > 0 ? unit.health : Mathf.maxZero(unit.maxHealth + unit.health)) / unit.maxHealth * 1.2 - 0.6), unit.y + (unit.hitSize / 2));
     Lines.stroke(2);
     if(unit.shield > 0){
-        for(let didgt = 1; didgt <= Mathf.digits(Mathf.floor(unit.shield / unit.maxHealth)) + 1; didgt++){
+        for(let didgt = 1; didgt <= Mathf.digits(unit.shield / unit.maxHealth) + 1; didgt++){
+            //if(didgt == Mathf.digits((int)(unit.shield / unit.maxHealth)) + 1) continue;
             Draw.color(Pal.shield, 0.8);
-            let shieldAmountScale = unit.shield / (unit.maxHealth * Mathf.pow(10, didgt - 1));
+            let barLength = Mathf.mod(unit.shield / unit.maxHealth, Mathf.pow(10, didgt - 1)) / Mathf.pow(10, didgt - 1);
             if(didgt > 1){
-                Lines.line(unit.x - unit.hitSize * 0.6, 
-                unit.y + (unit.hitSize / 2) + didgt * 2, 
-                unit.x + unit.hitSize * ((Mathf.ceil((shieldAmountScale - Mathf.floor(shieldAmountScale)) * 10) - 1 + 0.0001) * 1.2 * (1 / 9) - 0.6), 
-                unit.y + (unit.hitSize / 2) + didgt * 2);
-                //(s-1)*(1/9)because line(0) will draw length of 1
+                //let x = unit.x - (1 - Mathf.floor(barLength * 10) / 10) / 2 * 1.2 * unit.hitSize;
+                let y = unit.y + unit.hitSize / 2 + didgt * 2;
+                //let w = 1.2 * Mathf.floor(barLength * 10) / 10 * unit.hitSize;
+                for(let i = 1; i <= Mathf.floor(barLength * 10); i++){
+                    Fill.rect(unit.x - 0.55 * unit.hitSize + (i - 1) * 0.12 * unit.hitSize, y, 0.1 * unit.hitSize, 2);
+                }
             }else{
-                Lines.line(unit.x - unit.hitSize * 0.6, 
-                unit.y + (unit.hitSize / 2) + didgt * 2, 
-                unit.x + unit.hitSize * ((shieldAmountScale - Mathf.floor(shieldAmountScale) - 0.001) * 1.2 - 0.6), 
-                unit.y + (unit.hitSize / 2) + didgt * 2);
+                let x = unit.x - (1 - barLength) / 2 * 1.2 * unit.hitSize;
+                let y = unit.y + unit.hitSize / 2 + didgt * 2;
+                let w = 1.2 * barLength * unit.hitSize;
+                Fill.rect(x, y, w, 2);
             }
         }
     }
-    Draw.reset();
     
     var index = 0;
     for(let effi = 0; effi < content.statusEffects().size; effi++){
