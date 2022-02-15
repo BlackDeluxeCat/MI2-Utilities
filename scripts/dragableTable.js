85dc3028b4f5d5c5dc85518b08af4561e92fe775
BlackDeluxeCat <65377021+BlackDeluxeCat@users.noreply.github.com>
2022-02-15 23:29:58 +0800

    Mindow snap & better minimized

--- scripts/dragableTable.js
+++ scripts/dragableTable.js
@@ -1,10 +1,19 @@
 let currTopmost = null;
+let titleStyleNormal, titleStyleSnapped;
+/** Author: BlackDeluxeCat
+ * A dragable table that partly works like a window. 
+ * cont is a container for user items. 
+ * rebuildCont() can be set(with js) as a member method, which will be called when rebuild().
+ */
 module.exports={
     new:function(titleText){
+        titleStyleNormal = new Label.LabelStyle(Fonts.def, new Color(0.8,0.9,1,1));
+        titleStyleSnapped = new Label.LabelStyle(Fonts.def, new Color(0.1,0.6,0.6,1));
+
         let table = extend(Table, Styles.flatDown, {
-            curx : 0, cury : 0, fromx : 0, fromy : 0,
+            curx : 0, cury : 0, fromx : 0, fromy : 0, aboveSnap : null,
             titleText : null, titleButtons : null, cont : new Table(),
-            topmost : false, minimized : false, closable : true,
+            topmost : false, minimized : false, closable : true, 
 
             rebuild(){
                 this.clear();
@@ -17,17 +26,28 @@ module.exports={
                 if(!this.minimized) this.add(this.cont).colspan(2);
             },
 
+            /** return the js-extended table, not arc.scene.ui.layout.Table */
+            jsSelf(){
+                return table;
+            },
+
             setupTitle(){
                 let title = new Label(this.titleText);
                 this.add(title).update(() => {
                     this.curx = Mathf.clamp(this.curx, 0, (this.hasParent ? this.parent.getWidth() : Core.graphics.getWidth()) - this.getWidth());
                     this.cury = Mathf.clamp(this.cury, 0, (this.hasParent ? this.parent.getHeight() : Core.graphics.getHeight()) - this.getHeight());
+                    if(this.aboveSnap != null){
+                        this.curx = this.aboveSnap.jsSelf().curx;
+                        this.cury = this.aboveSnap.jsSelf().cury - this.getHeight();
+                    }
                     this.setPosition(this.curx, this.cury);
                     this.keepInStage();
                     this.invalidateHierarchy();
                     this.pack();
                     if(this == currTopmost) this.setZIndex(1000);
+                    title.setStyle(this.aboveSnap == null ? titleStyleNormal : titleStyleSnapped);
                 }).growX().fillY();
+                title.name = "MI2Drag";
                 title.setAlignment(Align.center);
                 title.addListener(extend(InputListener, {
                     touchDown(event, x, y, pointer, button){
@@ -37,15 +57,21 @@ module.exports={
                     },
                     touchDragged(event, x, y, pointer){
                         let v = table.localToStageCoordinates(Tmp.v1.set(x, y));
-                        table.curx = v.x - table.fromx;
-                        table.cury = v.y - table.fromy;
+                        let hit = Core.scene.hit(v.x + title.x, v.y + title.y, false);
+                        if(hit != null && hit.hasParent() && hit.name == "MI2Drag" && hit != title && hit.parent.jsSelf().aboveSnap != table){
+                            table.aboveSnap = hit.parent;
+                        }else{
+                            table.aboveSnap = null;
+                            table.curx = v.x - table.fromx;
+                            table.cury = v.y - table.fromy;
+                        }
                     }
                 }));
 
                 this.titleButtons = new Table();
                 this.titleButtons.button(String.fromCharCode(Iconc.refresh), Styles.cleart, () => {
                     this.rebuild();
-                }).size(36, 36);
+                }).size(24, 24);
                 this.titleButtons.button(String.fromCharCode(Iconc.lock), Styles.clearToggleMenut, () => {
                     this.topmost = !this.topmost;
                     if(this.topmost){
@@ -61,6 +87,12 @@ module.exports={
                 this.titleButtons.button("-", Styles.clearToggleMenut, () => {
                     this.minimized = !this.minimized;
                     this.rebuild();
+                    if(this.minimized){
+                        this.cury += this.cont.getHeight();
+                    }else{
+                        this.cury -= this.cont.getHeight();
+                    }
+                    
                 }).size(24, 24).update(b => {
                     b.setChecked(this.minimized);
                 });
